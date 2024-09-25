5a7649807cd184163b7eb0d8fd7fde1a287090af
Daviti0410 <lazariashvilidato@gmail.com>
2024-09-25 15:14:09 +0400

    Splited picture containers into components and gave background styling with props

---
+++ src/Cards.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export function Box1(props) {
+  return (
+    <div className="box" style={{ backgroundColor: props.backgroundColor }}>
+      <img src="/public/jeans.jpg" alt="Description1" className="image" />
+      <p>Description of Line 1</p>
+      <button type="button">Add to Cart</button>
+    </div>
+  );
+}
+
+export function Box2(props) {
+  return (
+    <div className="box" style={{ backgroundColor: props.backgroundColor }}>
+      <img src="./public/sneakers.jpg" alt="Description2" className="image" />
+      <p>Description of Line 2</p>
+      <button type="button">Add to Cart</button>
+    </div>
+  );
+}
+
+export function Box3(props) {
+  return (
+    <div className="box" style={{ backgroundColor: props.backgroundColor }}>
+      <img src="/public/T-shirt.jpg" alt="Description3" className="image" />
+      <p>Description of Line 3</p>
+      <button type="button">Add to Cart</button>
+    </div>
+  );
+}
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,29 +1,14 @@
 import React from "react";
 import "./Content.css";
+import { Box1, Box2, Box3 } from "./Cards";
 
 export default function Content() {
   return (
     <main className="outer-container">
       <div className="container">
-        <div className="box">
-          <img src="/public/jeans.jpg" alt="Description1" className="image" />
-          <p>Description of Line 1</p>
-          <button type="button">Add to Cart</button>
-        </div>
-        <div className="box">
-          <img
-            src="./public/sneakers.jpg"
-            alt="Description2"
-            className="image"
-          />
-          <p>Description of Line 2</p>
-          <button type="button">Add to Cart</button>
-        </div>
-        <div className="box">
-          <img src="/public/T-shirt.jpg" alt="Description3" className="image" />
-          <p>Description of Line 3</p>
-          <button type="button">Add to Cart</button>
-        </div>
+        <Box1 backgroundColor="green" />
+        <Box2 backgroundColor="red" />
+        <Box3 backgroundColor="blue" />
       </div>
     </main>
   );
