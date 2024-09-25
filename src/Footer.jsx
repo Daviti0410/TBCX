dc794cfb0f068c455eba3f21b88a233cd3b76de6
Daviti0410 <lazariashvilidato@gmail.com>
2024-09-25 14:48:41 +0400

    replaced main content with scrollable container with pictures, description and button

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,23 +2,21 @@ import "./Footer.css";
 
 export default function footer() {
   return (
-    <footer>
-      <div class="footer-section">
-        <ul class="footer-links">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About Us</a>
-          </li>
-          <li>
-            <a href="#">Services</a>
-          </li>
-          <li>
-            <a href="#">Contact Us</a>
-          </li>
-        </ul>
-      </div>
+    <footer class="footer-section">
+      <ul class="footer-links">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About Us</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Contact Us</a>
+        </li>
+      </ul>
     </footer>
   );
 }
