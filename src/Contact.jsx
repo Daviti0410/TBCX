1fca08a0e07aa32594c0cef6b1ed3bb630bec891
Daviti0410 <lazariashvilidato@gmail.com>
2024-09-29 02:37:28 +0400

    improved styling in Contact.css also implemented react-icons

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,24 +1,58 @@
 import "./Contact.css";
+import { MdOutlineModeOfTravel } from "react-icons/md";
+import { FaPhoneFlip } from "react-icons/fa6";
+import { IoMail } from "react-icons/io5";
 
 export default function Contact() {
   return (
     <main className="outer-contact-container">
       <h2>Contact Us</h2>
-      <form className="inner-contact-container">
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input type="text" placeholder="Jhon" name="name" />
+      <div className="middle-content-container">
+        <div className="contact-info">
+          <h2>Mail</h2>
+          <p>
+            We are here to help and answer any questions you might have. we are
+            looking forward to hearing from you.
+          </p>
+          <div className="inner-contact-info">
+            <p>
+              <MdOutlineModeOfTravel /> J. Shartavas 42, Tbilissi, Georgia
+            </p>
+            <p>
+              <FaPhoneFlip /> +995 591941921
+            </p>
+            <p>
+              <IoMail /> <a>lazariashvilidato@gmail.com</a>
+            </p>
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="email">email</label>
-          <input type="email" placeholder="JhonWick@example.com" name="email" />
+        <div className="gap">
+          <h1>OR</h1>
         </div>
-        <div className="form-group">
-          <label htmlFor="message">Message</label>
-          <textarea type="message" placeholder="Text Message" name="message" />
-        </div>
-        <button>submit</button>
-      </form>
+        <form className="inner-contact-container">
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" placeholder="Jhon" name="name" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">email</label>
+            <input
+              type="email"
+              placeholder="JhonWick@example.com"
+              name="email"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message</label>
+            <textarea
+              type="message"
+              placeholder="Text Message"
+              name="message"
+            />
+          </div>
+          <button>submit</button>
+        </form>
+      </div>
     </main>
   );
 }
