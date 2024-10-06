d96dc06749fc128aeb08519675e0af20e5bd35a9
MerabSamkharadze <Samkharadzemerab@gmail.com>
2024-10-06 16:20:34 +0400

    add posts from API and create dynamic rout for each post

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,38 +1,32 @@
+"use client";
+
 import "./Blog.css";
 
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
 export default function Blog() {
-  const blogPosts = [
-    {
-      imgSrc: "../public/bill-board.jpg",
-      altText: "Billboard Image",
-      title: "Billboard",
-      description:
-        "Best known for the Hot 100 and Billboard 200, which list the most popular songs and albums each week in the industry. Offers industry news, events, podcasts, and music streaming.",
-    },
-    {
-      imgSrc: "/public/business-insider.jpg",
-      altText: "Business Insider Image",
-      title: "Business Insider",
-      description:
-        "High-end business journalism keeping readers up-to-date on economic news as well as interviews with top entrepreneurs. There’s also educated predictions, trend analyses, and tips.",
-    },
-    {
-      imgSrc: "/public/people.jpg",
-      altText: "People Magazine Image",
-      title: "People",
-      description:
-        "Covers all things showbusiness, including celebrity gossip, entertainment news, and the latest on new shows, movies, and popular books.",
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = () => {
+    axios.get("https://dummyjson.com/posts").then((result) => {
+      setPosts(result.data.posts);
+    });
+  };
+
+  useEffect(getPosts, []);
   return (
     <main className="blog-container">
       <div className="blog-inner-container">
-        {blogPosts.map((post, index) => (
-          <div className="blog-list" key={index}>
-            <img src={post.imgSrc} alt={post.altText} />
+        {posts.map((post) => (
+          <div className="blog-list" key={post.id}>
             <div className="blog-content">
               <h2>{post.title}</h2>
-              <p>{post.description}</p>
+              <p>{post.body}</p>
+              <Link className="Link" href={`/blog/${post.id}`}>
+                Open Post
+              </Link>
             </div>
           </div>
         ))}
