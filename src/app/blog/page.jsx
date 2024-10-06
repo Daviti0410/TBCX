1beaf3186fe1257d35aec6cb528b72fda48f0f1a
MerabSamkharadze <Samkharadzemerab@gmail.com>
2024-10-06 21:56:54 +0400

    add loader on blog page, add styles on blog and dynamic post pages

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import "./Blog.css";
+import Loading from "@/components/Loading/Loading";
 
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -8,28 +9,35 @@ import Link from "next/link";
 
 export default function Blog() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getPosts = () => {
     axios.get("https://dummyjson.com/posts").then((result) => {
       setPosts(result.data.posts);
+      setLoading(false);
     });
   };
 
   useEffect(getPosts, []);
+
   return (
     <main className="blog-container">
       <div className="blog-inner-container">
-        {posts.map((post) => (
-          <div className="blog-list" key={post.id}>
-            <div className="blog-content">
-              <h2>{post.title}</h2>
-              <p>{post.body}</p>
-              <Link className="Link" href={`/blog/${post.id}`}>
-                Open Post
-              </Link>
+        {loading ? (
+          <Loading />
+        ) : (
+          posts.map((post) => (
+            <div className="blog-list" key={post.id}>
+              <div className="blog-content">
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+                <Link className="Link" href={`/blog/${post.id}`}>
+                  Open Post
+                </Link>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </main>
   );
