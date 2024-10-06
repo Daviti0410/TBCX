55b467586d67d3780b53b399f5f7157c4e4c22ed
SabaShagidze2 <154365489+SabaShagidze2@users.noreply.github.com>
2024-10-06 19:18:23 +0400

    Fetch products with axios, updated props, products page works

--- src/app/content/page.jsx
+++ src/app/content/page.jsx
@@ -1,13 +1,44 @@
+"use client";
+
 import "./Content.css";
-import { Box1, Box2, Box3 } from "../../components/Cards/Cards";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ProductCard } from "../../components/Cards/Cards";
 
 export default function Content() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const result = await axios.get("https://dummyjson.com/products");
+
+        setProducts(result.data.products);
+      } catch (error) {
+        console.error("Error fetching products", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <main className="outer-container">
       <div className="container">
-        <Box1 backgroundColor="green" />
-        <Box2 backgroundColor="red" />
-        <Box3 backgroundColor="blue" />
+        {products.length > 0 ? (
+          products.map((product) => (
+            <ProductCard
+              key={product.id}
+              title={product.title}
+              price={product.price}
+              image={product.thumbnail}
+              description={product.description}
+            />
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </main>
   );
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,42 +1,13 @@
-import PropTypes from "prop-types";
+"use client";
 
-export function Box1(props) {
+export function ProductCard({ title, price, image, description }) {
   return (
-    <div className="box" style={{ backgroundColor: props.backgroundColor }}>
-      <img src="/public/jeans.jpg" alt="Description1" className="image" />
-      <p>Description of Line 1</p>
+    <div className="box">
+      <img src={image} alt="Description1" className="image" />
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <p>{price}</p>
       <button type="button">Add to Cart</button>
     </div>
   );
 }
-
-export function Box2(props) {
-  return (
-    <div className="box" style={{ backgroundColor: props.backgroundColor }}>
-      <img src="./public/sneakers.jpg" alt="Description2" className="image" />
-      <p>Description of Line 2</p>
-      <button type="button">Add to Cart</button>
-    </div>
-  );
-}
-
-export function Box3(props) {
-  return (
-    <div className="box" style={{ backgroundColor: props.backgroundColor }}>
-      <img src="/public/T-shirt.jpg" alt="Description3" className="image" />
-      <p>Description of Line 3</p>
-      <button type="button">Add to Cart</button>
-    </div>
-  );
-}
-Box1.propTypes = {
-  backgroundColor: PropTypes.string,
-};
-
-Box2.propTypes = {
-  backgroundColor: PropTypes.string,
-};
-
-Box3.propTypes = {
-  backgroundColor: PropTypes.string,
-};
