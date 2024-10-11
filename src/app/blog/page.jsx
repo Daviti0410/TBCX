7fa01cad37c467b23f5dfc5d80b247e4c498d5d5
Daviti0410 <lazariashvilidato@gmail.com>
2024-10-11 15:30:45 +0400

    გავაიყვანე server side- კომპონენტად პოსტების და პროდუქტების სიები

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,42 +1,35 @@
-"use client";
-
 import "./Blog.css";
 import Loading from "@/components/Loading/Loading";
 import axios from "axios";
-import { useEffect, useState } from "react";
 import Link from "next/link";
 
-export default function Blog() {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
+export async function getPosts() {
+  try {
+    const results = await axios.get("https://dummyjson.com/posts");
+    const data = results.data;
+    return data.posts;
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return [];
+  }
+}
 
-  const getPosts = () => {
-    axios.get("https://dummyjson.com/posts").then((result) => {
-      setPosts(result.data.posts);
-      setLoading(false);
-    });
-  };
-
-  useEffect(getPosts, []);
-
+export default async function Blog() {
+  const posts = await getPosts();
   return (
     <main className="blog-container">
       <div className="blog-inner-container">
-        {loading ? (
-          <Loading />
-        ) : (
-          posts.map((post) => (
-            <div className="blog-list" key={post.id}>
-              <div className="blog-content">
-                <h2>{post.title}</h2>
-                <p>{post.body}</p>
-                <Link className="Link" href={`/blog/${post.id}`}>
-                  Open Post
-                </Link>
-              </div>
+        {posts.map((post) => (
+          <div className="blog-list" key={post.id}>
+            <div className="blog-content">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+              <Link className="Link" href={`/blog/${post.id}`}>
+                Open Post
+              </Link>
             </div>
-          ))
-        )}
+          </div>
+        ))}
       </div>
     </main>
   );
