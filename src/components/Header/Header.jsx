08e0a1de20c50b010699236f64f5a95c3fbb6147
Daviti0410 <lazariashvilidato@gmail.com>
2024-11-04 18:07:44 +0400

    დავამატე ნათელი და მუქი თემები ასევე პირველ ჩატვირთვაზე ავტომატურად ცვლის სისტემის თემაზე, დავამატე თეილვინდი და რამოდენიმე გვერდი გავსტილე

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,50 @@
-"use client";
-
 import Link from "next/link";
 import "./Header.css";
+import ThemeToggle from "../ThemeToggle/ThemeToggle";
 
 export default function Header() {
   return (
     <header>
-      <div className="header-container">
+      <div className="flex justify-center items-center mx-[15%] dark:bg-[#1f2937] bg-[#1f2937] h-28 rounded-b-full">
         <div className="logo">
           <Link href="/">MyWebsite</Link>
         </div>
         <nav className="navbar">
           <ul className="nav-list">
             <li>
-              <Link href="/about">About</Link>
+              <Link href="/about" className="hover:text-[#3b82f6]">
+                About
+              </Link>
             </li>
             <li>
-              <Link href="/blog">Blog</Link>
+              <Link href="/blog" className="hover:text-[#3b82f6]">
+                Blog
+              </Link>
             </li>
             <li>
-              <Link href="/contact">Contact</Link>
+              <Link href="/contact" className="hover:text-[#3b82f6]">
+                Contact
+              </Link>
             </li>
             <li>
-              <Link href="/products">Produts</Link>
+              <Link href="/products" className="hover:text-[#3b82f6]">
+                Produts
+              </Link>
             </li>
             <li>
-              <Link href="/profile">Profile</Link>
+              <Link href="/profile" className="hover:text-[#3b82f6]">
+                Profile
+              </Link>
             </li>
+            <ThemeToggle />
           </ul>
         </nav>
+        <a
+          className="w-8  text-2xl dark:text-white dark:hover:text-blue-400 text-white hover:text-blue-400  font-bold text-center py-3 px-4 cursor-pointer  "
+          href="/api/auth/logout"
+        >
+          LogOut
+        </a>
       </div>
     </header>
   );
