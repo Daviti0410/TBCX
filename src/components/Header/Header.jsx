2a5f6e61b2845449d77261e9797ad64e94b35555
Daviti0410 <lazariashvilidato@gmail.com>
2024-11-15 16:12:55 +0400

    ავტირთე ინტერნაცინალიზაცია მაგრამ middleware არ მუშაობს ისე როგორც საჭიროა

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
+"use client";
 import Link from "next/link";
 import "./Header.css";
 import ThemeToggle from "../ThemeToggle/ThemeToggle";
+import { useState } from "react";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const changeLanguage = (locale) => {
+    setIsOpen(false);
+
+    router.push(`/${locale}${pathname.slice(3)}`);
+  };
   return (
     <header>
       <div className="flex justify-center items-center mx-[15%] dark:bg-[#1f2937] bg-[#1f2937] h-28 rounded-b-full">
@@ -45,6 +54,30 @@ export default function Header() {
         >
           LogOut
         </a>
+        <div className="relative">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="bg-gray-700 text-white p-2 rounded-md border border-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          >
+            {pathname.includes("/ka") ? "ქართული" : "English"}
+          </button>
+          {isOpen && (
+            <div className="absolute bg-white text-black shadow-lg rounded-md mt-2 w-40 overflow-hidden">
+              <button
+                onClick={() => changeLanguage("en")}
+                className="w-full text-left px-4 py-2 hover:bg-gray-200"
+              >
+                English
+              </button>
+              <button
+                onClick={() => changeLanguage("ka")}
+                className="w-full text-left px-4 py-2 hover:bg-gray-200"
+              >
+                ქართული
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </header>
   );
