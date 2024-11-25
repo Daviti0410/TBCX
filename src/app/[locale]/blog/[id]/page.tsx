33aa255c8839c65b7552579a55e84d163a834706
Daviti0410 <lazariashvilidato@gmail.com>
2024-11-25 18:11:39 +0400

    გადავიყვანე ბლოგების გვერდი თეილვინდზე

--- src/app/[locale]/blog/[id]/page.tsx
+++ src/app/[locale]/blog/[id]/page.tsx
@@ -26,10 +26,14 @@ export default async function PostDetail({ params }: PostDetailProps) {
   }
 
   return (
-    <div className="main-container">
-      <div className="post-container">
-        <h1 className="post-title">{post.title}</h1>
-        <p className="post-body">{post.description}</p>
+    <div className="flex flex-col items-center content-center min-h-[75vh]">
+      <div className="flex flex-col content-center items-center p-10 border-8 border-solid border-white max-w-[800px] my-0 m-auto">
+        <h1 className="text-4xl font-bold text-white mb-5 text-center">
+          {post.title}
+        </h1>
+        <p className="text-lg leading-6 text-justify text-white">
+          {post.description}
+        </p>
       </div>
     </div>
   );
